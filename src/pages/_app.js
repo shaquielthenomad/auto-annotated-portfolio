49bd1db08d6c092aa9711e41ae69e472c70366f3
shaquielthenomad <2210112611001@live4.utcc.ac.th>
2025-01-21 12:00:33 +0700

    Update Layout and _app.js with new file structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
 import '../css/main.css';
 import { useEffect } from 'react';
+import Layout from '../components/Layout';
 
 export default function MyApp({ Component, pageProps }) {
     useEffect(() => {
-        console.log('Wix Client ID:', process.env.WIX_CLIENT_ID);
-
-        if (!process.env.WIX_CLIENT_ID) {
-            console.error('WARNING: Wix Client ID is not set in environment variables');
-        }
+        console.log('Wix Environment Variables:', {
+            CLIENT_ID: process.env.WIX_CLIENT_ID,
+            API_KEY: process.env.WIX_API_KEY ? 'Present' : 'Missing',
+            SITE_ID: process.env.WIX_SITE_ID,
+            ACCOUNT_ID: process.env.WIX_ACCOUNT_ID
+        });
     }, []);
 
-    return <Component {...pageProps} />;
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
