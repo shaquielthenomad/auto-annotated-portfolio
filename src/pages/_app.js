e0a52f14eb8860ae1de76e99cf5d08f96a4646be
shaquielthenomad <2210112611001@live4.utcc.ac.th>
2025-01-21 11:41:10 +0700

    Add Wix Headless integration with client ID and store component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '../css/main.css';
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        console.log('Wix Client ID:', process.env.WIX_CLIENT_ID);
+
+        if (!process.env.WIX_CLIENT_ID) {
+            console.error('WARNING: Wix Client ID is not set in environment variables');
+        }
+    }, []);
+
     return <Component {...pageProps} />;
 }
