37a34c69d910fce245aead62c61191ebd987e43d
shaquielthenomad <2210112611001@live4.utcc.ac.th>
2025-01-22 23:28:26 +0700

    Add type annotations to FeaturedProjectsSection

--- src/components/sections/FeaturedProjectsSection/index.tsx
+++ src/components/sections/FeaturedProjectsSection/index.tsx
@@ -1,4 +1,18 @@
+import React from 'react';
+import { BaseProps } from '@/types/common';
 import ProjectFeedSection from '../ProjectFeedSection';
-export default function FeaturedProjectsSection(props) {
-    return <ProjectFeedSection {...props} />;
+
+interface Project {
+  title?: string;
+  description?: string;
+  image?: string;
+}
+
+interface FeaturedProjectsSectionProps extends BaseProps {
+  title?: string;
+  projects?: Project[];
+}
+
+export default function FeaturedProjectsSection(props: FeaturedProjectsSectionProps) {
+  return <ProjectFeedSection {...props} />;
 }
